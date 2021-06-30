d6a65d6af5413191d9ff5614e6d90abc813ffd6d
Seyit Erdemir <seyiterdemir4242@gmail.com>
2021-06-30 11:46:08 +0300

    11.45

---
+++ resources/views/backend/default/index.blade.php
@@ -0,0 +1,18 @@
+
+@extends('backend.layout')
+
+@section('content')
+
+<section class="content-header">
+    <h1>
+        Page Header
+        <small>Optional description</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+    </ol>
+</section>
+@endsection
+@section('css')@endsection
+@section('js')@endsection
