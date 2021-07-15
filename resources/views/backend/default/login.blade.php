4e7979f18a96d0bd3b59e708d5bb71b020dddd8b
Seyit Erdemir <seyiterdemir4242@gmail.com>
2021-07-15 13:55:28 +0300

    15.07.2021  14.00

--- resources/views/backend/default/login.blade.php
+++ resources/views/backend/default/login.blade.php
@@ -1,119 +1,121 @@
 <!DOCTYPE html>
-<html>
+<html lang="tr">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 2 | Log in</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="/backend/bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="/backend/bower_components/font-awesome/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="/backend/bower_components/Ionicons/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="/backend/dist/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="/backend/plugins/iCheck/square/blue.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-
-    <!-- Google Font -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <style type="text/css">
-    .login-page {
-        background: url(/images/settings/xxxxx.jpg) no-repeat center center fixed;
-        background-size: cover;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    }
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" type="text/css" href="/backend/login/vendor/bootstrap/css/bootstrap.min.css">
 
-    body {
-        overflow: hidden;
-    }
-    </style>
+    <link rel="stylesheet" type="text/css" href="/backend/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 
+    <link rel="stylesheet" type="text/css" href="/backend/login/vendor/animate/animate.css">
+
+    <link rel="stylesheet" type="text/css" href="/backend/login/vendor/css-hamburgers/hamburgers.min.css">
+
+    <link rel="stylesheet" type="text/css" href="/backend/login/vendor/select2/select2.min.css">
+
+    <link rel="stylesheet" type="text/css" href="/backend/login/css/util.css">
+    <link rel="stylesheet" type="text/css" href="/backend/login/css/main.css">
+
+    <meta name="robots" content="noindex, follow">
 </head>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-            <a href="{{route('nedmin.Login')}}" style="color: white;"><b>...</b>Yazılım</a>
-        </div>
-        <!-- /.login-logo -->
-        <div class="login-box-body">
-            <p class="login-box-msg">Kullanıcı ve Şifrenizi Giriniz</p>
-           @if(Session::has('error'))
-                <div class="alert alert-danger">
-                  {{session('error')}}
+<body>
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-pic js-tilt" data-tilt="">
+                    <a href="{{route('nedmin.Login')}}"><img src="/images/settings/img-01.png" alt="IMG"></a>
                 </div>
-                @elseif(Session::has('success'))
-                <div class="alert alert-success">
-                  {{session('success')}}
-                </div>
-              
-            @endif
 
-            <form action="{{route('nedmin.Authenticate')}}" method="post">
-                @csrf
-                <div class="form-group has-feedback">
-                    <input type="email" required class="form-control" name="email" value="{{old('email')}}" placeholder="Kullanıcı Adı">
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="password" required class="form-control"  name="password"  placeholder="Şifre">
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <div class="row">
-                    <div class="col-xs-8">
-                        <div class="checkbox icheck">
-                            <label>
-                                <input name="remember_me" {{old('remember_me') ? 'checked' : ' ' }} type="checkbox"> Remember Me
-                            </label>
-                        </div>
+                <form action="{{route('nedmin.Authenticate')}}" method="post" class="login100-form validate-form">
+                    @csrf
+
+                    @if(Session::has('error'))
+                    <div class="alert alert-danger">
+                        {{session('error')}}
                     </div>
-                    <!-- /.col -->
-                    <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Giriş Yap</button>
+                    @elseif(Session::has('success'))
+                    <div class="alert alert-success">
+                        {{session('success')}}
                     </div>
-                    <!-- /.col -->
-                </div>
-            </form>
+                    @endif
+                    <span class="login100-form-title">
+                        Yönetici Girişi
+                    </span>
+                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                        <input class="input100" required type="text" name="email" placeholder="Kullanıcı Adı"
+                            value="{{old('email')}}">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" required type="password" name="password" placeholder="Parolanız..">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
 
 
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+                    <div class="text-center p-t-136">
 
 
+                        <i class="  m-l-5" aria-hidden="true"></i>
+
+                    </div>
+
+
+                </form>
+            </div>
         </div>
-        <!-- /.login-box-body -->
     </div>
-    <!-- /.login-box -->
 
-    <!-- jQuery 3 -->
-    <script src="/backend/bower_components/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="/backend/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- iCheck -->
-    <script src="/backend/plugins/iCheck/icheck.min.js"></script>
+    <script src="/backend/login/vendor/jquery/jquery-3.2.1.min.js"></script>
+
+    <script src="/backend/login/vendor/bootstrap/js/popper.js"></script>
+    <script src="/backend/login/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="/backend/login/vendor/select2/select2.min.js"></script>
+
+    <script src="/backend/login/vendor/tilt/tilt.jquery.min.js"></script>
     <script>
-    $(function() {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' /* optional */
-        });
-    });
+    $('.js-tilt').tilt({
+        scale: 1.1
+    })
+    </script>
+
+    <script async="" src="../../../gtag/js.js?id=UA-23581568-13"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-23581568-13');
+    </script>
+
+    <script src="js/main.js"></script>
+    <script defer="" src="../../../beacon.min.js"
+        data-cf-beacon='{"rayId":"66f1b64eaff9fd01","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.6.0","si":10}'>
     </script>
 </body>
 
