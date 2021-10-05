07df4607479187e9e22300ad1cb12af37150481b
Seyit Erdemir <seyiterdemir4242@gmail.com>
2021-10-05 17:35:02 +0300

    5.10.21

--- resources/views/backend/default/login.blade.php
+++ resources/views/backend/default/login.blade.php
@@ -1,3 +1,119 @@
+<!doctype html>
+<html lang="tr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/backend/login/fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="/backend/login/css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/backend/login/css/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="/backend/login/css/style.css">
+
+    <title>Hoşgeldin</title>
+</head>
+
+<body>
+
+
+
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="/backend/login/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-md-6 contents">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="mb-4">
+                                <h3>Yönetici Girişi</h3>
+
+                            </div>
+                            <form action="{{route('nedmin.Authenticate')}}" method="post" class="login100-form validate-form">
+                                @csrf
+
+                                @if(Session::has('error'))
+                                <div class="alert alert-danger">
+                                    {{session('error')}}
+                                </div>
+                                @elseif(Session::has('success'))
+                                <div class="alert alert-success">
+                                    {{session('success')}}
+                                </div>
+                                @endif
+                                <div class="form-group first">
+                                    <label for="username">Ad Soyad</label>
+                                    <input type="text" class="form-control" id="username" name="email"  >
+
+                                </div>
+                                <div class="form-group last mb-4">
+                                    <label for="password">Parola</label>
+                                    <input   class="form-control" id="password"  required type="password" name="password"   >
+
+                                </div>
+ 
+
+                                <input type="submit" value="Giriş Yap" class="btn btn-block btn-primary">
+
+                              
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <script src="/backend/login/js/jquery-3.3.1.min.js"></script>
+    <script src="/backend/login/js/popper.min.js"></script>
+    <script src="/backend/login/js/bootstrap.min.js"></script>
+    <script src="/backend/login/js/main.js"></script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
+    eski giriş bölümü css dosyaları silindi
+    
 <!DOCTYPE html>
 <html lang="tr">
 
@@ -119,4 +235,4 @@
     </script>
 </body>
 
-</html>
+</html> -->
